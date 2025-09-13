d2a3608855c0c205b4b0473ffa802026f4e0a854
Tribhuwan Tiwari <your_github_email@example.com>
2025-09-13 16:26:09 +0200

    chore: fix package.json and add sitemap config

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://radheshyamtt.com',
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+  sitemapSize: 5000
+}
